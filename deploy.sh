f6c7fe8c108e0a2450031c49ce65f1c51a028b7d
Michał Terczyński <mterczynski1@gmail.com>
2024-12-16 01:39:45 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@ rm -rf ../mterczynski.github.io/assets
 rm -rf ../mterczynski.github.io/index.html
 cp ./index.html ../mterczynski.github.io/
 cp -r ./assets ../mterczynski.github.io/
-cd ../mterczynski.github.io/pattern-generators/
+cd ../mterczynski.github.io
 # 2. Commit in ../mterczynski.github.io
+git add .
 git commit -a -m "Update project-showcase code and assets: $(git log -1 --pretty=format:"%s")"
 git status
 # 3. Push in ../mterczynski.github.io
