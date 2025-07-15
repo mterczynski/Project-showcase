123c4e0bc1658969472c3bde8925f4ee171a8d44
Michał Terczyński <mterczynski1@gmail.com>
2025-07-15 23:57:25 +0200

    Remove double webiste view count logger

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,15 +3,12 @@ import Commercial_projects from "@/components/Commercial_projects";
 import Message from "@/components/Message";
 import Nav from "@/components/Nav";
 import PersonalProjects from "@/components/PersonalProjects";
-import { updateLandingPageViews } from "@/lib/updateview";
 
 export async function generateStaticParams() {
   return [{ locale: "en" }, { locale: "pl" }];
 }
 
 export default async function Home() {
-  const newCount = await updateLandingPageViews();
-  console.log(`${newCount} views`);
   return (
     <>
       <Nav />
