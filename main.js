86fdbcb10170e7e8c09ad8ecbc069ca7d7633a4f
Chuks <63956312+skido600@users.noreply.github.com>
2026-03-14 19:29:50 +0100

    Add new changes v2

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ const translations = {
       title: "O mnie",
       intro:
         "Witam, nazywam się Michał Terczyński, jestem Frontend Developerem ze specjalizacją w tworzeniu gier i aplikacji internetowych. Zdobyłem doświadczenie w renomowanych firmach takich jak NetEnt, Yggdrasil Gaming i Software Mind, gdzie poszerzyłem wiedzę w zakresie tworzenia angażujących gier oraz wielomodułowych aplikacji biznesowych z wykorzystaniem TypeScript, React, Angular i PixiJS.",
-      certifications:
-        "Posiadam certyfikaty: AWS Certified Cloud Practitioner, AWS Certified AI Practitioner, Microsoft Certified: Azure Fundamentals oraz Google Cloud Certified: Generative AI Leader.",
+      certifications: "Uzyskane certyfikaty",
       details:
         "Moje umiejętności techniczne obejmują nowoczesne frameworki JavaScript, tworzenie gier w PixiJS oraz budowanie skalowalnych aplikacji internetowych. Pasjonuję się tworzeniem wysokiej jakości, wydajnego kodu i chętnie podejmuję się zarówno projektów komercyjnych, jak i autorskich.",
     },
@@ -74,8 +73,7 @@ const translations = {
       title: "About Me",
       intro:
         "Hi, I'm Michał Terczyński, a Frontend Developer with a strong focus on game development and interactive applications. With experience at companies like NetEnt, Yggdrasil Gaming, and Software Mind, I've developed expertise in creating engaging games and robust enterprise applications using TypeScript, React, Angular, and PixiJS.",
-      // certifications:
-      //   "I hold the ollowing certifications: AWS Certified Cloud Practitioner, AWS Certified AI Practitioner, Microsoft Certified: Azure Fundamentals, and Google Cloud Certified: Generative AI Leader.",
+      certifications: "Obtained Certifications",
       details:
         "My technical skills include modern JavaScript frameworks, game development with PixiJS, and building scalable web applications. I'm passionate about creating high-quality, performant code and enjoy working on both commercial projects and personal game development endeavors.",
     },
@@ -145,7 +143,8 @@ document.getElementById("aboutTitle").innerText = t.about.title;
 document.getElementById("aboutIntro").innerText = t.about.intro;
 document.getElementById("aboutCertificates").innerText = t.about.certifications;
 document.getElementById("aboutDetails").innerText = t.about.details;
-
+document.getElementById("certification_title").innerText =
+  t.about.certifications;
 document.getElementById("commercial-projects-title").innerText =
   t.projects.commercial;
 document.getElementById("personal-projects-title").innerText =
