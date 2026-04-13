2b1ee217c9d51c7d67d534c029da84dc5de55bc8
Michał Terczyński <mterczynski1@gmail.com>
2026-04-13 22:31:09 +0200

    Remove grid logic temporarily

--- utils/grid-Logic.js
+++
@@ -1,8 +0,0 @@
-export function chunkArray(arr, sizes) {
-  let index = 0;
-  return sizes.map((size) => {
-    const chunk = arr.slice(index, index + size);
-    index += size;
-    return chunk;
-  });
-}
